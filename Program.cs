afb576ec59856aa8da004062352dcb4e5c0ae327
ZussiMane <igoryunja@gmail.com>
2024-03-16 13:51:41 +0300

    Добавил 2 решения задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,35 @@
 // [1 -5 6]
 // => [-1 5 -6]
 
+// int[] array = new int[10] { -10, -7 - 5, -4, -1, 0, 1, 4, 5, 7, 10 };
+// int length = array.Length;
+// int index = 0;
+
+// while (index < length)
+// {
+//     // array[index] = array[index] * -1; // возможное написание
+//     array[index] *= -1;
+//     Console.Write(array[index] + " ");
+//     index++;
+// }
+//-------усовершенственное решение-------//
+int[] array = new int[10] { -10, -7, -5, -4, -1, 0, 4, 5, 7, 10 };
+int index = 0;
+int length = array.Length;
+
+for (int i = 0; i < array.Length; i++) // Печатаем массив с ',' и последний элемент без ','
+{
+    if (i < length - 1)
+        Console.Write($"{array[i]}, ");
+    else
+        Console.Write(array[i]);
+}
+Console.WriteLine();
+
+while (index < length)
+{
+    // array[index] = array[index] * -1;
+    array[index] *= -1;
+    Console.Write(array[index] + " ");
+    index++;
+}
